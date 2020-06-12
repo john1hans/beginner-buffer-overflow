3e252c494d868892d802ac80755e03216c4615dc
john1hans <66824663+john1hans@users.noreply.github.com>
2020-06-12 08:47:58 -0400

    Create after.c

---
+++ after.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+   char yourname[1];
+   int yourage[1];
+
+    printf("Whats your name?\t");
+    scanf("%1[^\n]s", yourname);
+
+    yourname[1] = '\0';
+    fflush(stdin);
+
+    printf("How old are you?\t");
+    scanf(" %d",&yourage);
+
+    printf("You are %d years old and your name is %s\n\n\n", yourage, yourname);
+
+    return(0);
+}
